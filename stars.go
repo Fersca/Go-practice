ffc40cd0b3c2853314f58928c915bc5b56e5a7e6
fersca <fersca@hotmail.com>
2016-05-11 20:40:35 -0300

    improve

--- stars.go
+++ stars.go
@@ -8,10 +8,25 @@ import (
 )
 
 func main() {
+	for i := 0; i < 50; i++ {
+		go star()
+	}
+	star()
+}
+
+func star() {
+
+	row := 0
+	col := 0
+	color := 0
 
 	for {
-		fmt.Printf("\033[" + strconv.Itoa(rand.Intn(24)) + ";" + strconv.Itoa(rand.Intn(80)) + "H\x1b[3" + strconv.Itoa(rand.Intn(8)) + ";1m" + "*")
-		time.Sleep(20 * time.Millisecond)
+		fmt.Printf("\033[" + strconv.Itoa(row) + ";" + strconv.Itoa(col) + "H ")
+		row = rand.Intn(24)
+		col = rand.Intn(80)
+		color = rand.Intn(8)
+		fmt.Printf("\033[" + strconv.Itoa(row) + ";" + strconv.Itoa(col) + "H\x1b[3" + strconv.Itoa(color) + ";1m" + "*")
+		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
 	}
 
 }
