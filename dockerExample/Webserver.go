1af170e189b2f93ba6d041b0880ad81ca0ef5ece
Fernando Scasserra <fersca@hotmail.com>
2016-09-01 15:47:37 -0300

    Write and read from rabbit and enable running outside the a container

--- dockerExample/Webserver.go
+++ dockerExample/Webserver.go
@@ -4,18 +4,139 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-
+	"os"
 	"github.com/bradfitz/gomemcache/memcache"
+	"github.com/streadway/amqp"
 )
 
+//Memcached client
 var mc *memcache.Client
 
+//Rabbit Connection
+var rabbitConn *amqp.Connection
+var rabbitChannel *amqp.Channel
+var rabbitQueue amqp.Queue
+var rabbitErr error
+var rabbitChanDelivery <-chan amqp.Delivery
+
+var memcachedServer string = "192.168.99.100" //"memcached1"
+var rabbitServer string = "192.168.99.100" //"some-rabbit"
+
+//Handle the incomming request
 func handler(w http.ResponseWriter, r *http.Request) {
 
-	it, err := mc.Get("visita")
+	consume := r.URL.Query().Get("consume")
+
+	if consume == "" {
+		//Get the stored visits from memcached
+		it, err := mc.Get("visita")
+
+		//Check the quantity
+		quantity := getQuantity(it, err)
+
+		fmt.Fprintf(w, "Visita %s!", strconv.Itoa(quantity))
+		fmt.Println("Visita: ", strconv.Itoa(quantity))
+
+		visits := []byte(strconv.Itoa(quantity))
+
+		//Save the new value in memcached
+		mc.Set(&memcache.Item{Key: "visita", Value: visits})
+
+		//Publish the visit number to rabbit
+		sendRabbit(visits)
+	} else {
+		//Consume from the queue
+		strMessage := consumeRabbit()
+		fmt.Fprintf(w, "Mensaje consumido: %s!", strMessage)
+	}
+
+}
+
+func main() {
+
+	fmt.Println("Iniciando Webserver....")
+	hostname := os.Getenv("HOSTNAME")
+	if hostname!="" {
+		fmt.Println("Running from Container")
+		memcachedServer = "memcached1"
+		rabbitServer = "some-rabbit"		
+	} else {
+		fmt.Println("Running from Localhost")
+	}
+
+	//Esta IP es la IP del container, también se puede poner la de la máquina virtual y funciona
+	//(que es más práctico porque es siempre la misma para todos los containers), la otra es poner el nomber del link
+
+	//Create the memcached client with the container name linked
+	mc = memcache.New(memcachedServer+":11211")
+
+	//Create the Rabbit connection
+	initRabbitMQ()
+
+	//Creathe the webserver
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		fmt.Println("Msg: ", msg, "Error: ", err)
+	}
+}
+
+func initRabbitMQ() {
+
+	//Create the connection with the rabbit cluster
+	rabbitConn, rabbitErr = amqp.Dial("amqp://guest:guest@"+rabbitServer+":5672/")
+	failOnError(rabbitErr, "Failed to connect to RabbitMQ")
+
+	//Creathe the channel
+	rabbitChannel, rabbitErr = rabbitConn.Channel()
+	failOnError(rabbitErr, "Failed to open a channel")
+
+	//Creathe the queue
+	rabbitQueue, rabbitErr = rabbitChannel.QueueDeclare(
+		"visits", // name
+		false,    // durable
+		false,    // delete when unused
+		false,    // exclusive
+		false,    // no-wait
+		nil,      // arguments
+	)
+	failOnError(rabbitErr, "Failed to declare a queue")
+
+	//Create the channel to consume the events
+	rabbitChanDelivery, rabbitErr = rabbitChannel.Consume(
+		rabbitQueue.Name, // queue
+		"",               // consumer
+		false,            // auto-ack
+		false,            // exclusive
+		false,            // no-local
+		false,            // no-wait
+		nil,              // args
+	)
+
+}
+
+func sendRabbit(data []byte) {
+
+	rabbitErr = rabbitChannel.Publish(
+		"",               // exchange
+		rabbitQueue.Name, // routing key
+		false,            // mandatory
+		false,            // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        data,
+		})
+
+	fmt.Println("Sent to rabbit! bytes: ", len(data))
+	failOnError(rabbitErr, "Failed to publish a message")
+}
+
+func getQuantity(it *memcache.Item, err error) int {
 
 	quantity := 0
-
 	if err == nil && it != nil {
 		xs := string(it.Value)
 		x, _ := strconv.Atoi(xs)
@@ -24,20 +145,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Error", err)
 	}
 
-	quantity++
-	fmt.Fprintf(w, "Visita %s!", strconv.Itoa(quantity))
-	fmt.Println("Visita: ", strconv.Itoa(quantity))
-
-	mc.Set(&memcache.Item{Key: "visita", Value: []byte(strconv.Itoa(quantity))})
-
+	return quantity
 }
 
-func main() {
-	fmt.Println("Iniciando Webserver....")
+func consumeRabbit() string {
 
-	//Esta IP es la IP del container, también se puede poner la de la máquina virtual y funciona 
-	//(que es más práctico porque es siempre la misma para todos los containers)
-	mc = memcache.New("memcached1:11211")
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	failOnError(rabbitErr, "Failed to register a consumer")
+
+	fmt.Println("Esperando Mensaje... ")
+	
+	message := <- rabbitChanDelivery
+
+	message.Ack(false)
+
+	fmt.Println("Mensaje recibido: ", string(message.Body))
+	return string(message.Body)
+			
 }
--- dockerExample/correr.sh
+++ dockerExample/correr.sh
@@ -1,12 +1,14 @@
 #Crea el docker para memcached
-#docker run --detach --name memcached1 memcached 
+#docker run --detach --name memcached1 memcached
+#docker run --detach --name memcached1 --publish=11211:11211 memcached 
 docker start memcached1
 
-#Esto es lo que tenía antes el memcached, publicaba puertos, ahora no hace falta porque está linkeado
-#docker run --detach --name memcached1 --publish=11211:11211 memcached
+#Corre el rabbit, usa una imagen que tiene la consola de operaciones
+#docker run -d --hostname my-rabbit --name some-rabbit -p 15672:15672 -p 5672:5672 rabbitmq:3-management
+docker start some-rabbit
 
 #Compila el código del webserver y lo mete en una imagen
-#docker build -t my-golang-app .
+docker build -t my-golang-app .
 
 #Crea un docker con la imagen recientemente creada y lo linkea al memcached1
-docker run --publish=8080:8080 --link memcached1 -it --rm --name my-running-app my-golang-app
+docker run --publish=8080:8080 --link memcached1 --link some-rabbit -it --rm --name my-running-app my-golang-app
